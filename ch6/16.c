a4668a09ee75b5a97cd8420c8c2154e11b1808cf
fishBone000 <FishBoneEK@outlook.com>
2021-12-13 11:02:11 +0000

    休斯顿，我们遇到了点麻烦…… printf居然奇妙地报了malloc的错，研究一下 对应程序：ch6/16.c 应该用g++ -g -o 16 16.c就能复现了， 输入的时候打： 5 5 9 8 7 6 5 5 6 7 8 9 或者 2 2 6 7 7 6

--- ch6/16.c
+++ ch6/16.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int length;
-#define LENGTH length
+#define LENGTH_16 length
 
 
 void sort(int *list){
-	short flags[LENGTH] = {0}; // RUBBISH QINGLINE!!!
-	int result [LENGTH+1] = {1};
+	short *flags = (short*)calloc(LENGTH_16, sizeof(short)); // RUBBISH QINGLINE!!!
+	int *result = (int*)calloc(LENGTH_16+1, sizeof(int));
 
 	while(1){
 		int min, j=-1;
-		for(int i = 0; i < LENGTH; i++){
+		for(int i = 0; i < LENGTH_16; i++){
 			if(flags[i])
 				continue;
 			if(j==-1 || list[i] < min)
@@ -21,15 +22,18 @@ void sort(int *list){
 		flags[j] = 1;
 		result[result[0]++] = min;
 	}
-	for(int i = 0; i < LENGTH; i++)
-		printf("%d ", result[i+1]);
+	free(flags);
+	free(result);
 	return;
 }
 
 int main(){
 	int m, n;
 	scanf("%d %d", &m, &n);
-	int listA[m], listB[n];
+	int *listA = (int*)calloc(m, sizeof(int));
+	int *listB = (int*)calloc(n, sizeof(int));
+	int *reserveA = listA;
+	int *reserveB = listB;
 
 	for(int i = 0; i < m; i++)
 		scanf("%d", listA+i);
@@ -40,23 +44,28 @@ int main(){
 		int *p_temp, temp;
 		p_temp = listA;
 		temp = m;
-		listA = (int*)listB;
+		listA = listB;
 		m = n;
 		listB = p_temp;
 		n = temp;
 	}
 
-	int result[m+1] = {0};
+	int *result = (int*)calloc(m+1, sizeof(int));
 
 	for(int i = 0; i < m; i++)
 		for(int j = 0; j < n; j++)
-			if(listA[i] == listB[i])
-				result[++result[0]] = listA[m];
-	LENGTH = result[0];
-	if(!LENGTH)
-		return 0;
+			if(listA[i] == listB[j])
+				result[++result[0]] = listA[i];
+	LENGTH_16 = result[0];
+	if(!LENGTH_16)
+		goto end;
 	sort(result+1);
-	for(int i = 0; i < LENGTH; i++)
-		printf("%d ", result[i+1]);
+	for(int i = 0; i < LENGTH_16; i++){
+		int element = result[i+1];
+		fprintf(stdout, "%d ", element);
+	}
+	end: free(reserveA);
+	     free(reserveB);
+	     free(result);
 	return 0;
 }
